afe9c3645997b8ba220d2607ed9a39c70fe02602
Chris Murphy <_chrism_@live.com>
2018-04-06 11:55:38 +0100

    created Record

--- Record.js
+++ Record.js
@@ -1,1 +1,8 @@
 const _ = require( 'lodash');
+
+const Record = function(artist, title, genre, price){
+  this.artist = artist;
+  this.title = title;
+  this.genre = genre;
+  this.price = price;
+}
