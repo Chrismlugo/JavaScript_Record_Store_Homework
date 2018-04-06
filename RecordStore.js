752ee1806c90abfe0fb176e9c4a4ab7738c8d417
Chris Murphy <_chrism_@live.com>
2018-04-06 12:31:01 +0100

    passes test can add records

--- RecordStore.js
+++ RecordStore.js
@@ -4,10 +4,15 @@ const RecordStore = function(name,city){
   this.name = name;
   this.city = city;
   this.inventory = [];
+  this.balance = 0;
 }
 
-RecordStore.prototype.countRecords = function (record) {
+RecordStore.prototype.countRecords = function () {
   return this.inventory.length;
+};
+
+RecordStore.prototype.addRecord = function (record) {
+  this.inventory.push(record);
 };
 
 module.exports = RecordStore;
